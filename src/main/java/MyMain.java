86b99f8d817d50b4b3537c60b7910c88be6dbc27
koyo01xp <21kasakawa@athenian.org>
2020-11-05 20:52:05 +0000

    homework

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,24 +4,59 @@ public class MyMain {
 
     // Counts the number of odd numbers in list
     public static int countOdd(ArrayList<Integer> list) { 
-        // REPLACE WITH YOUR CODE
-        return 0;
+        int c = 0;
+        for(int i = 0;i < list.size();i++){
+            int x = list.get(i);
+            if (x % 2 != 0){
+                c++;
+            }
+        }
+        return c;
     }
-
     // Returns true if there is an int that appears in both lists
     public static boolean checkDuplicates(ArrayList<Integer> list1, ArrayList<Integer> list2) { 
-        // REPLACE WITH YOUR CODE
+        for(int i = 0;i < list1.size();i++){
+            int x = list1.get(i);
+            for(int a = 0;a<list2.size();a++){
+                int y = list2.get(a);
+                if (x==y) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     // Takes an int[] as input and returns the equivalent ArrayList<Integer>
     public static ArrayList<Integer> convertToArrayList(int[] arr) { 
-        // REPLACE WITH YOUR CODE
-        return null;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i = 0;i<arr.length;i++){
+            list.add(arr[i]);
+        }
+        return list;
     }
 
 
     public static void main(String[] args) {
-        // Write some code here to test your methods!
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(countOdd(list));
+
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(checkDuplicates(list, list2));
+
+        int [] arr = {0,1,2,3,4,5};
+        System.out.println(convertToArrayList(arr));
+
     }
 }
